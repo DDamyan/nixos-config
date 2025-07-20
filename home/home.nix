01f43d5b406a59d74e1bc546b8185510f9ad2777
DDamyan <ddminedd@gmail.com>
2025-07-20 19:17:17 +0200

    🏷️ added bash alias

--- home/home.nix
+++ home/home.nix
@@ -7,31 +7,33 @@
 }:
 {
 
-    home.packages = with pkgs; [ 
+  home.packages = with pkgs; [
     nixfmt-rfc-style # formater for nix
     nil # language server for nix
-      firefox
-      kate
-      vscode
-      neovim
-    ];
+    firefox
+    kate
+    vscode
+    neovim
+  ];
 
 
-    home.shellAliases = {
-        nrs = "sudo nixos-rebuild switch --flake /home/damyan/nixos-config#";
-        nrt = "sudo nixos-rebuild test --flake /home/damyan/nixos-config#";
-    };
+  home.shellAliases = {
+    nrs = "sudo nixos-rebuild switch --flake /home/damyan/nixos-config#";
+    nrt = "sudo nixos-rebuild test --flake /home/damyan/nixos-config#";
+    nup = "nix flake update --flake /home/damyan/nixos-config#";
+  };
 
-    home.username = "damyan";
-    home.homeDirectory = "/home/damyan";
+  home.username = "damyan";
+  home.homeDirectory = "/home/damyan";
 
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
-    home.stateVersion = "23.11";
+  programs.bash.enable = true;
+  home.stateVersion = "23.11";
 
-    programs.git = {
-        enable = true;
-        userEmail = "ddminedd@gmail.com";
-        userName = "DDamyan";
-    };
+  programs.git = {
+    enable = true;
+    userEmail = "ddminedd@gmail.com";
+    userName = "DDamyan";
+  };
 }
